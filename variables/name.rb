28faf8ef49d7e714ec6bce571bc48185129d7004
Aydyn <aydyn112304@gmail.com>
2020-04-13 21:16:33 +0000

    Variables chapter complete

---
+++ variables/name.rb
@@ -0,0 +1,13 @@
+puts "Whats your first name?"
+
+firstname = gets.chomp
+
+puts "Whats your last name?"
+
+lastname = gets.chomp
+
+puts "Hello " + firstname + " " + lastname 
+
+10.times do 
+  puts firstname + " " + lastname
+end
